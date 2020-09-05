9cddc4e944d5b56ed4399d51c7b1123688da086b
Hiro Suzuki <hiro.suzuki.oct10@gmail.com>
2020-09-05 14:27:45 +0000

    GOGC=50

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@
 export ISUBATA_DB_HOST=10.146.15.201
 export ISUBATA_DB_USER=isucon
 export ISUBATA_DB_PASSWORD=isucon
+export GOGC=50
 cd /home/isucon/isubata/webapp/go
 exec ./isubata > /tmp/isubata.log 2>/tmp/isubata-err.log
 
